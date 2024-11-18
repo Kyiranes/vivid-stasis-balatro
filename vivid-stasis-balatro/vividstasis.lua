fc58aba9a12622d9904fab262f39793650a5622f
Kyiranes <evilmcchip@gmail.com>
2024-11-18 18:24:18 -0500

    Fixed Nurse

--- vivid-stasis-balatro/vividstasis.lua
+++ vivid-stasis-balatro/vividstasis.lua
@@ -65,7 +65,7 @@ SMODS.Joker{
         if context.joker_main then
             return {
                 mult_mod = card.ability.extra.mult,
-                message = localize { type = 'variable', key = 'a_mult', vars = {card.ability.extra.mult - 5} }
+                message = localize { type = 'variable', key = 'a_mult', vars = {card.ability.extra.mult} }
             }
         end
         if context.before and not context.blueprint then
